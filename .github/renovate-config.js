ee84261a1c91b9e09c1c698b5f161caac4a5a9a9
Malte Jan Kaffenberger <1078035+maltejk@users.noreply.github.com>
2021-06-01 23:29:43 +0200

    chore: don't require config

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -3,6 +3,7 @@ module.exports = {
   dryRun: true,
   gitAuthor: 'Renovate Bot <github-renovate@maltejk.de>',
   onboarding: false,
+  requireConfig: false,
   platform: 'github',
   repositories: [
     'maltejk/docker-borgbackup',
