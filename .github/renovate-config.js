5d4d3ed77fde1d92d6d4705c570e284ea7ea1e34
Malte Jan Kaffenberger <1078035+maltejk@users.noreply.github.com>
2021-06-02 20:52:08 +0200

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -2,6 +2,7 @@ module.exports = {
   branchPrefix: 'renovate/',
   gitAuthor: 'Renovate Bot <github-renovate@maltejk.de>',
   onboarding: false,
+  requireConfig: false,
   platform: 'github',
   repositories: [
     'maltejk/docker-borgbackup',
